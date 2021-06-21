34b20a070901ce148662facaeef307893e64ca01
fabezio <fabezio@outlook..fr>
2021-06-21 09:55:52 +0200

    connextion à Mongo Atlkas

--- server/serve.js
+++ server/serve.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 // const { urlencoded } = require('body-parser')
+const router = express.Router()
 
 const app = express()
 app.use(cors())
@@ -10,14 +11,16 @@ app.use(bodyParser.json())
 
 // dbhandler
 const {model, connect, Schema}=mongoose
-const db="personal"
+const db="timemap"
+const pw="C0denCQRT!"
+const distUrl= `mongodb+srv://fabezio:${pw}@cluster0.0r1tc.mongodb.net/${db}?retryWrites=true&w=majority`
 const localUrl = `mongodb://localhost/${db}`
 
-connect(localUrl, {
+connect(distUrl, {
     useCreateIndex: true,
     useUnifiedTopology: true,
     useNewUrlParser: true
-})
+}).then(() => console.log(`Connected to mongodb, database: ${db}`))
 
 const userSch = new Schema({
     firstname: String,
@@ -27,21 +30,21 @@ const userSch = new Schema({
 
 const User = model('User', userSch)
 
-// const newUser = new User({ firstname: "fabio", lastname: "boutentrain", email: "faboutentrain@free.fr" })
+// const newUser = new User({ firstname: "toto", lastname: "dupont", email: "toot-dupont@outlook.fr" })
 // newUser.save()
 
 app.get('/', (_, res) => {
     res.send('<a href="/users" >Users</a>')
 })
-
-app.get('/users', (_, res) => {
+app.use('/users', router)
+router.route('/').get((_, res) => {
     User.find({}, (err, user) => {
         err
             ? res.status(400).send('No found')
             : res.status(200).send(user)
         })
     })
-app.get('/users/:id', (req, res) => {
+router.route('/:id').get((req, res) => {
     User.findOne({_id: req.params.id}, (err, item) => {
         err
         ? res.status(400).send('No found')
